5cfbad9767fac5215360ed464560458bd8941082
borea <a.jizdann@gmail.com>
2017-12-20 10:43:51 +0100

    aggiunta la pagina di login

---
+++ public/home.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <title>Facebook Theme Demo</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+      <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <link href="assets/css/home.css" rel="stylesheet">
+    <script src="https://use.fontawesome.com/1e803d693b.js"></script>
+</head>
+
+<body>
+<div class="wrapper">
+      <header class="main-head">The header</header>
+      <nav class="main-nav">
+          <ul>
+              <li><a href="">Nav 1</a></li>
+              <li><a href="">Nav 2</a></li>
+              <li><a href="">Nav 3</a></li>
+          </ul>
+      </nav>
+      <article class="content">
+            <h1>Main article area</h1>
+          <p>In this layout, we display the areas in source order for any screen less that 500 pixels wide. We go to a two column layout, and then to a three column layout by redefining the grid, and the placement of items on the grid.</p>
+      </article>
+      <aside class="side">Sidebar</aside>
+      <div class="ad">Advertising</div>
+      <footer class="main-footer">The footer</footer>
+</div>
+</body>
+</html>
