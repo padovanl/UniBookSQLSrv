6e7415617305bb9266ea33d02f1e52d206e9a7e1
daniele <lvtdnl@gmail.com>
2018-01-04 18:23:46 +0100

    -Caricamento dei post delle pagine seguite; -Caricamento dei post degli amici e personali in ordine temporale; -Ora i post funzionano; -Creati due controller "Search" e "Profile" con le relative views;
    Per ora il caricamento dei post è tutto server based, infatti viene
    aggiornata la pagina ogni volta che metto un post.
    Le mie prossime priorità sono:
    -finire la ricerca (backend);
    -caricamento likes (backend);
    -Caricamento post con Ajax;
    -Post e Like con Ajax.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,223 +1,123 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="wrapper">
-    <div class="box">
-        <div class="row row-offcanvas row-offcanvas-left">
+  <div class="padding">
+      <div class="full col-sm-12">
+          <!-- content -->
+          <div class="row">
+              <!-- main col right -->
+              <div class="col-sm-7">
 
-            <!-- sidebar -->
-            <div class="column col-sm-2 col-xs-1 sidebar-offcanvas" id="sidebar">
-
-                <ul class="nav">
-                    <li><a href="#" data-toggle="offcanvas" class="visible-xs text-center"><i class="glyphicon glyphicon-chevron-right"></i></a></li>
-                </ul>
-
-                <ul class="nav hidden-xs" id="lg-menu">
-                    <li class="active"><a href="#featured"><i class="glyphicon glyphicon-list-alt"></i> Featured</a></li>
-                    <li><a href="#stories"><i class="glyphicon glyphicon-list"></i> Stories</a></li>
-                    <li><a href="#"><i class="glyphicon glyphicon-paperclip"></i> Saved</a></li>
-                    <li><a href="#"><i class="glyphicon glyphicon-refresh"></i> Refresh</a></li>
-                </ul>
-                <ul class="list-unstyled hidden-xs" id="sidebar-footer">
-                    <li>
-                        <a href="https://www.facebook.com/unife.it/"><h3>UniBook</h3> <i class="glyphicon glyphicon-heart-empty"></i> by UniFe</a>
-                    </li>
-                </ul>
-
-                <!-- tiny only nav-->
-                <ul class="nav visible-xs" id="xs-menu">
-                    <li><a href="#featured" class="text-center"><i class="glyphicon glyphicon-list-alt"></i></a></li>
-                    <li><a href="#stories" class="text-center"><i class="glyphicon glyphicon-list"></i></a></li>
-                    <li><a href="#" class="text-center"><i class="glyphicon glyphicon-paperclip"></i></a></li>
-                    <li><a href="#" class="text-center"><i class="glyphicon glyphicon-refresh"></i></a></li>
-                </ul>
-
-            </div>
-            <!-- /sidebar -->
-
-            <!-- main right col -->
-            <div class="column col-sm-10 col-xs-11 col-offeset-4" id="main">
-
-                <!-- top nav -->
-                <div class="navbar navbar-blue navbar-static-top">
-                    <div class="navbar-header">
-                        <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-collapse">
-                            <span class="sr-only">Toggle</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <a href="/" class="navbar-brand logo">b</a>
+                  <div class="well">
+                      <form class="form" method="POST" action="/post">
+                          <h4>New Post</h4>
+                          <div class="input-group text-center">
+                            <input name="_token" type="hidden" value="{{ csrf_token() }}">
+                              <input class="form-control input-lg" name="content" placeholder="Hey, What's Up?" type="text">
+                              <span class="input-group-btn"><button type="submit" class="btn btn-lg btn-primary">Post</button></span>
+                          </div>
+                      </form>
+                  </div>
+                  <!--Caricamento dei post-->
+                  @foreach ($posts as $post)
+                  <?php $author = $controller->ShowUser($post['id_author']); ?>
+                  <!--Nuovo pannello commenti-->
+                  <div class="panel panel-default">
+                    <div class="panel-heading"><ul class="nav navbar-nav navbar-right">
+                          <li class="dropdown">
+                              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                <i class="glyphicon glyphicon-user"></i>
+                              </a>
+                              <ul class="dropdown-menu">
+                                  <li><a href="">Dettagli Post</a></li>
+                                  <li><a href="">Visualizza Profilo</a></li>
+                                  <li><a href="">Rimuovi dagli amici</a></li>
+                                  <li><a href="">Segnala Post</a></li>
+                              </ul>
+                          </li>
+                      </ul>
+                      <div>
+                          <h4>{{$controller->PrintName($post['id_author'])}}</h4>
+                          <img src="{{$author['pic_path']}}" class="img-circle pull-left">
+                      </div>
                     </div>
-                    <nav class="collapse navbar-collapse" role="navigation">
-                        <form class="navbar-form navbar-left">
-                            <div class="input-group input-group-sm" style="max-width:360px;">
-                                <input class="form-control" placeholder="Search" name="srch-term" id="srch-term" type="text">
-                                <div class="input-group-btn">
-                                    <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-                                </div>
-                            </div>
-                        </form>
-                        <ul class="nav navbar-nav">
-                            <li>
-                                <a href="#"><i class="glyphicon glyphicon-home"></i> Home</a>
-                            </li>
-                            <li>
-                                <a href="#postModal" role="button" data-toggle="modal"><i class="glyphicon glyphicon-plus"></i> Post</a>
-                            </li>
-                            <li>
-                                <a href="#"><span class="badge">{{$user -> name}} {{$user -> surname}}</span></a>
-                            </li>
-                        </ul>
-                        <ul class="nav navbar-nav navbar-right">
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="glyphicon glyphicon-user"></i></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="">Report..</a></li>
-                                    <li><a href="">Activity Log</a></li>
-                                    <li><a href="">Settings</a></li>
-                                    <li><a href="">About UniBook</a></li>
-                                    <li><a href="/logout">Logout</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-                <!-- /top nav -->
+                    <div class="panel-body">
+                    <p>{{$post['content']}}</p>
+                    <div class="clearfix"></div>
+                    @foreach ($list_comments as $comment)
+                        @if ($comment['id_post'] === $post -> id_post)
+                        <!--linea-->
+                        <hr>
+                        <!--TODO: immagine-->
+                        <p><a href="/profile/user/{{$comment['id_author']}}">{{$controller->PrintName($comment -> id_author)}}  </a>{{$comment -> content}}</p>
+                      @endif
+                  @endforeach
+                        <hr>
+                    <form>
+                    <div class="input-group">
+                      <div class="input-group-btn">
+                      <button class="btn btn-default">+1</button><button class="btn btn-default"><i class="glyphicon glyphicon-share"></i></button>
+                      </div>
+                      <input class="form-control" placeholder="Add a comment.." type="text">
+                    </div>
+                    </form>
 
-                <div class="padding">
-                    <div class="full col-sm-9">
+                    </div>
+                 </div>
+                @endforeach
 
-                        <!-- content -->
-                        <div class="row">
+              </div>
+              <div class="col-sm-5">
+                  <div class="container">
+                      <div class="row">
+                          <div class="panel panel-default user_panel">
+                              <div class="panel-heading">
+                                  <h3 class="panel-title">Suggested Friends</h3>
+                              </div>
+                              <div class="panel-body">
+                                  <div class="table-container">
+                                      @foreach ($suggested_friends as $suggested)
+                                      <table class="table-users table" border="0">
+                                          <tbody>
+                                          <tr>
+                                              <td  align="center">
+                                                  <i class="fa fa-2x fa-user fw"></i>
+                                              </td>
+                                              {{$suggested -> name . ' ' . $suggested -> surname}}
+                                              <td>
+                                              </td>
 
-                            <!-- main col right -->
-                            <div class="col-sm-7">
-
-                                <div class="well">
-                                    <form class="form" method="POST" action="/post">
-                                        <h4>New Post</h4>
-                                        <div class="input-group text-center">
-                                          <input name="_token" type="hidden" value="{{ csrf_token() }}">
-                                            <input class="form-control input-lg" name="content" placeholder="Hey, What's Up?" type="text">
-                                            <span class="input-group-btn"><button type="submit" class="btn btn-lg btn-primary">Post</button></span>
-                                        </div>
-                                    </form>
-                                </div>
-                                @foreach ($posts as $post)
-                                <?php $u = $controller->ShowUser($post[0] -> id_author); ?>
-                                  <div class="panel panel-default">
-                                      <div class="panel-heading"><ul class="nav navbar-nav navbar-right">
-                                              <li class="dropdown">
-                                                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                                    <i class="glyphicon glyphicon-user"></i>
-                                                  </a>
-                                                  <ul class="dropdown-menu">
-                                                      <li><a href="">More</a></li>
-                                                      <li><a href="">More</a></li>
-                                                      <li><a href="">More</a></li>
-                                                      <li><a href="">More</a></li>
-                                                      <li><a href="">More</a></li>
-                                                  </ul>
-                                              </li>
-                                          </ul> <h4>
-
-                                          </h4>
-                                        </div>
-                                      <div class="panel-body">
-                                          <div class="clearfix"></div>
-                                          <hr>
-                                          {{$post[0] -> content}}
-                                        </hr>
-                                      </div>
-                                      @foreach ($list_comments as $post_comments)
-                                        @foreach ($post_comments as $comment)
-                                          @if ($comment['id_post'] === $post[0] -> id_post)
-                                      <div class="panel-comment">
-                                        <div class="clearfix"></div>
-                                        <hr>
-                                          {{$comment -> content}}
-                                      </hr>
-                                      </div>
-                                        @endif
-                                      @endforeach
+                                          </tr>
+                                          </tbody>
+                                      </table>
                                     @endforeach
                                   </div>
-                                @endforeach
-                            </div>
-                            <div class="col-sm-5">
-                                <div class="container">
-                                    <div class="row">
-                                        <div class="panel panel-default user_panel">
-                                            <div class="panel-heading">
-                                                <h3 class="panel-title">Suggested Friends</h3>
-                                            </div>
-                                            <div class="panel-body">
-                                                <div class="table-container">
-                                                    @foreach ($friends as $friend)
-                                                    <table class="table-users table" border="0">
-                                                        <tbody>
-                                                        <tr>
-                                                            <td  align="center">
-                                                                <i class="fa fa-2x fa-user fw"></i>
-                                                            </td>
-                                                            <td>
-                                                                <br><i class="fa fa-envelope"></i>
-                                                            </td>
-                                                            {{$friend -> name}}
-                                                            <td>
-                                                            </td>
-                                                            <td align="center">
-                                                                Last Login:  6/14/2017<br><small class="text-muted">2 days ago</small>
-                                                            </td>
-                                                        </tr>
-                                                        </tbody>
-                                                    </table>
-                                                  @endforeach
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div><!--/row-->
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div><!--/row-->
 
+          <div class="row">
+              <div class="col-sm-6">
+                  <a href="#">Twitter</a> <small class="text-muted">|</small> <a href="#">Facebook</a> <small class="text-muted">|</small> <a href="#">Google+</a>
+              </div>
+          </div>
 
-                        <div class="row">
-                            <div class="col-sm-6">
-                                <a href="#">Twitter</a> <small class="text-muted">|</small> <a href="#">Facebook</a> <small class="text-muted">|</small> <a href="#">Google+</a>
-                            </div>
-                        </div>
+          <div class="row" id="footer">
+              <div class="col-sm-6"/>
+          </div>
+          <hr>
+          <h4 class="text-center">
+              <a href="/" target="ext">UniBook</a>
+          </h4>
+          <hr>
+      </div><!-- /col-9 -->
+  </div><!-- /padding -->
+@endsection
 
-                        <div class="row" id="footer">
-                            <div class="col-sm-6">
-
-                            </div>
-                            <div class="col-sm-6">
-                                <p>
-                                    <a href="#" class="pull-right">\A9Copyright 2013</a>
-                                </p>
-                            </div>
-                        </div>
-
-                        <hr>
-
-                        <h4 class="text-center">
-                            <a href="/" target="ext">Download this Template @Bootply</a>
-                        </h4>
-
-                        <hr>
-
-                    </div><!-- /col-9 -->
-                </div><!-- /padding -->
-            </div>
-            <!-- /main -->
-
-        </div>
-    </div>
-</div>
-
-
+@section('post')
 <!--post modal-->
 <div id="postModal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog">
