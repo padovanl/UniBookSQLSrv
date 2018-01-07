7221e3c5f52e8d6e02495449022c39a6d88747e2
Luca Padovan <luca.padovan@student.unife.it>
2018-01-07 17:46:55 +0100

    Fix Message factory

--- database/factories/MessageFactory.php
+++ database/factories/MessageFactory.php
@@ -7,7 +7,7 @@ $factory->define(App\Message::class, function (Faker $faker) {
       $userIDs = DB::table('users')->pluck('id_user')->all();
       $letto = array(1 => true, 2 => false);
       return [
-            'content' => $faker->paragraph,
+            'content' => $faker->sentence(10),
             'sender' => $faker->randomElement($userIDs),
             'receiver' => $faker->randomElement($userIDs),
             'letto' => $faker->randomElement($letto),
