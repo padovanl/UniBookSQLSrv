20e8f9d9d01800668e0e3d058d980a0c2abd9633
daniele <lvtdnl@gmail.com>
2018-01-09 15:40:21 +0100

    -Commit con JS funzionante per load post

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,6 @@
   <article class="content">
   <div class="padding">
           <!-- content -->
-          <div class="row">
               <!-- main col right -->
                   <div class="well">
                       <form class="form" method="POST" action="/post">
@@ -42,14 +41,6 @@
                     <p id="post_content">Content</p>
                     <div id="insert_after" class="clearfix"></div>
                     <!--Comment Panel-->
-                    <!--div id="commpanel">
-                      <hr>
-
-                      <a id="author" href="Link_To_User">Comment User Name  </a>
-                      <p id="comm_details">
-                        Comment Body
-                      </p>
-                    </div!-->
                     <hr>
                     <!--Comment Form-->
                     <form>
@@ -60,11 +51,9 @@
                       <input class="form-control" placeholder="Add a comment.." type="text">
                     </div>
                     </form>
-
                     </div>
                  </div>
-                <button type="button"/>Load More..
-          </div><!--/row-->
+                <button id="load" onclick="loadOlder()" type="button"/>Load More..
           <hr>
   </div><!-- /padding -->
 </article>
@@ -73,66 +62,95 @@
 
   <script>
 
+  function onLoad(data){
+    $("#post").hide();
+    //ciclo su tutti i post, al contrario perchè prendo dal più recente al più vecchio
+    for(var i = data.length - 1; i >= 0; i--)
+    {
+      console.log(data.length);
+      $post_clone = $("#post").clone();
+      $post_clone.attr("id", "post_" + data[i].id_post);
+      $post_clone.find("#post_u_name").text(data[i].auth_name + " " + data[i].auth_surname);
+      $post_clone.find("#post_pic_path").attr('src', data[i].pic_path);
+      $post_clone.find("#post_content").text(data[i].content);
+      $post_clone.find("#like_butt").text(data[i].likes);
+      $post_clone.find("#insert_after").attr('id', "insert_after" + data[i].id_post);
+      $post_clone.insertAfter(".well");
+      $post_clone.show();
+      }
+      data.forEach(function(el) {
+        //carico i commenti
+        if(el.comments.length > 0 && el.id_post == el.comments[0].id_post){
+          for(j = 0; j < el.comments.length; j++){
+            $("<div id='commpanel" + el.comments[j].id_comment +
+               "'><hr><!--TODO: immagine-->" +
+               "<a id='author' href='/profile/user/" + el.comments[j].id_user + "'>" +
+               el.comments[j].auth_name + " " + el.comments[j].auth_surname +
+               "</a><p id='comm_details'>" + el.comments[j].content +"</p></div>").insertAfter("#insert_after" + el.id_post);
+      }
+    }
+  })
+  }
+
+  function loadOlder(){
+    $prev_post = $("#post").prev();
+    $post_id = $prev_post.attr("id").split("_")[1];
+    $.ajax({
+            method: "GET",
+            url: "/home/loadmore",
+            data: { post_id: $post_id },
+            dataType : "json",
+            success : function (posts)
+            {
+              if(posts.length != 0)
+              {
+                for(var x = posts.length - 1; x >= 0 ; x--)
+                {
+                  console.log(x);
+                  $post_clone = $("#post").clone();
+                  $post_clone.attr("id", "post_" + posts[x].id_post);
+                  $post_clone.find("#post_u_name").text(posts[x].auth_name + " " + posts[x].auth_surname);
+                  $post_clone.find("#post_pic_path").attr('src', posts[x].pic_path);
+                  $post_clone.find("#post_content").text(posts[x].content);
+                  $post_clone.find("#like_butt").text(posts[x].likes);
+                  $post_clone.find("#insert_after").attr('id', "insert_after" + posts[x].id_post);
+                  $post_clone.insertAfter("#post_" + $post_id);
+                  $post_clone.show();
+                  }
+                  posts.forEach(function(el) {
+                    //carico i commenti
+                    if(el.comments.length > 0 && el.id_post == el.comments[0].id_post){
+                      for(j = 0; j < el.comments.length; j++){
+                        $("<div id='commpanel" + el.comments[j].id_comment +
+                           "'><hr><!--TODO: immagine-->" +
+                           "<a id='author' href='/profile/user/" + el.comments[j].id_user + "'>" +
+                           el.comments[j].auth_name + " " + el.comments[j].auth_surname +
+                           "</a><p id='comm_details'>" + el.comments[j].content +"</p></div>").insertAfter("#insert_after" + el.id_post);
+                  }
+                }
+              })
+              }
+              else{
+                $("#load").text("Nothing More!");
+              }
+
+            }
+          })
+
+  }
+
   //Caricamento dei post
   $(document).ready(function(){
-         $.ajax({
-             url : '/home/loadmore',
-             method : "GET",
-             dataType : "json",
-             success : function (data)
-             {
-               $("#post").hide();
-               //ciclo su tutti i post, al contrario perchè prendo dal più recente al più vecchio
-               for(i = data.length - 1; i >= 0; i--)
-               {
-                 $post_clone = $("#post").clone();
-                 $post_clone.attr("id", "post_" + i);
-                 $post_clone.find("#post_u_name").text(data[i].auth_name + " " + data[i].auth_surname);
-                 $post_clone.find("#post_pic_path").attr('src', data[i].pic_path);
-                 $post_clone.find("#post_content").text(data[i].content);
-                 $post_clone.find("#like_butt").text(data[i].likes);
-                 $post_clone.find("#insert_after").attr('id', "insert_after" + data[i].id_post);
-                 $post_clone.insertAfter(".form");
-                 $post_clone.show();
-                 }
-                 data.forEach(function(el) {
-                   //carico i commenti
-                   if(el.comments.length > 0 && el.id_post == el.comments[0].id_post){
-                     for(j = 0; j < el.comments.length; j++){
-                       $("<div id='commpanel" + el.comments[j].id_comment +
-                          "'><hr><!--TODO: immagine-->" +
-                          "<a id='author' href='/profile/user/" + el.comments[j].id_user + "'>" +
-                          el.comments[j].auth_name + " " + el.comments[j].auth_surname +
-                          "</a><p id='comm_details'>" + el.comments[j].content +"</p></div>").insertAfter("#insert_after" + el.id_post);
-                 }
-               }
-             })
+       $.ajax({
+           url : '/home/loadmore',
+           method : "GET",
+           dataType : "json",
+           data: { post_id: -1 },
+           success : function (posts)
+           {
+             onLoad(posts);
            }
+       });
   });
-});
   </script>
 @endsection
-
-
-{{-- @section('suggested_friends')
-<h3 class="panel-title">Suggested Friends</h3>
-<div class="panel-body">
-    <div class="table-container">
-        @foreach ($suggested_friends as $suggested)
-        <table class="table-users table" border="0">
-            <tbody>
-            <tr>
-                <td  align="center">
-                    <i class="fa fa-2x fa-user fw"></i>
-                </td>
-                {{$suggested -> name . ' ' . $suggested -> surname}}
-                <td>
-                </td>
-
-            </tr>
-            </tbody>
-        </table>
-      @endforeach
-    </div>
-</div>
-@endsection --}}
