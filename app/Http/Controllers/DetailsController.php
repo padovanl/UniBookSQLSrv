38dfec93d7ff961ca55ba34e2fb05cbc111c9877
daniele <lvtdnl@gmail.com>
2018-01-12 16:56:53 +0100

    -fix

---
+++ app/Http/Controllers/DetailsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Cookie;
+use App\User;
+use App\Message;
+use App\Notification;
+
+class DetailsController extends Controller{
+
+  public function landing(){
+
+  }
+
+}
+?>
