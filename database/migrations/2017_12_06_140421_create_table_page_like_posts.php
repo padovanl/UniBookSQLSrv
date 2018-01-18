ba18093db4a50ba1cd3f6ad2b798aa55ab261d9f
Luca Padovan <luca.padovan@student.unife.it>
2018-01-18 15:15:41 +0100

    Fatte migrations per i like delle pagine

---
+++ database/migrations/2017_12_06_140421_create_table_page_like_posts.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTablePageLikePosts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pages_like_posts', function (Blueprint $table) {
+            //increments imposta automaticamente la chiave primaria, non serve usare il metodo primary()
+            $table->integer('id_post')->unsigned();
+            $table->integer('id_page')->unsigned();
+            $table->boolean('like');
+
+            //foreign key
+            $table->foreign('id_page')->references('id_page')->on('pages');
+            $table->foreign('id_post')->references('id_post')->on('posts');
+         });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pages_like_posts');
+    }
+}
