b0aa227ddb9fff6788f09b5466985fe1d7251b82
ArtPes <arturo.pesaro@gmail.com>
2018-01-18 16:15:45 +0100

    Bug

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -96,6 +96,7 @@
                       </div>
                   </div>
               </div>
+              <div class="insert_after_me"></div>
               <!--Pannello Post-->
               <div class="container" id="post">
                   <div class="row">
