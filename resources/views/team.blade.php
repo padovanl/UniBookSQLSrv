6b9d4864c1f1b33572fc10b952e0eac7e837969e
Luca Padovan <luca.padovan@student.unife.it>
2018-01-19 00:58:04 +0100

    Sistemato layout pagina sviluppatori

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -1,141 +1,68 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-html {
-  box-sizing: border-box;
-}
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 
-*, *:before, *:after {
-  box-sizing: inherit;
-}
-
-.column {
-  float: left;
-  width: 33.3%;
-  margin-bottom: 16px;
-  padding: 0 8px;
-}
-
-@media (max-width: 400px) {
-  .column {
-    width: 80%;
-    display: block;
-  }
-}
-
-.card {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-}
-
-.row {
-  height: 50%;
-}
-
-.container {
-  padding: 0 16px;
-}
-
-.container::after, .row::after {
-  content: "";
-  clear: both;
-  display: table;
-}
-
-.title {
-  color: grey;
-}
-
-.button {
-  border: none;
-  outline: 0;
-  display: inline-block;
-  padding: 8px;
-  color: white;
-  background-color: #000;
-  text-align: center;
-  cursor: pointer;
-  width: 100%;
-}
-
-.button:hover {
-  background-color: #555;
-}
-
-</style>
+<link rel="icon" href="/assets/img/icon2.png">
+<title>UniBook - Sviluppatori</title>
 </head>
 <body>
-
-<h2>Meet The Team!</h2>
-<p>Gruppo 9</p>
-<br>
-
-<div class="row">
-  <div class="column">
-    <div class="card">
-      <img src="/assets/team/Arturo_Pesaro.jpg" alt="Jane" style="width:100%">
-      <div class="container">
-        <h2>Arturo Pesaro</h2>
-        <p class="title">DevS</p
-        <p>Studente presso Università degli Studi di Ferrara</p>
-        <p>arturo.pesaro@student.unife.it</p>
-        <p><button class="button">Contatta</button></p>
-      </div>
+<br />
+<div class="container">
+  <h1 style="text-align: center;"><span class="badge badge-primary">Sviluppatori</span></h1>
+  <br />
+  <div class="row" style="width: 1000px; margin: 0 auto;">
+    <div class="col-md-4" style="text-align: center;">
+      <img src="/assets/team/Luca_Padovan.jpg" style="border-radius: 50%;" width="250px" class="mx-auto d-block" height="250px" />
+      <h3><span class="badge badge-primary">Luca Padovan</span></h3>
+    </div>
+    <div class="col-md-4" style="text-align: center;">
+      <img src="/assets/team/Daniele_Lovato.jpg" style="border-radius: 50%;" width="250px" class="mx-auto d-block" height="250px" />
+      <h3><span class="badge badge-primary">Daniele Lovato</span></h3>
+    </div>
+    <div class="col-md-4" style="text-align: center;">
+      <img src="/assets/team/Arturo_Pesaro.jpg" style="border-radius: 50%;" width="250px" class="mx-auto d-block" height="250px" />
+      <h3><span class="badge badge-primary">Arturo Pesaro</span></h3>
     </div>
   </div>
-
-  <div class="column">
-    <div class="card">
-      <img src="/assets/team/luca_padovan.jpg" alt="Mike" style="width:100%">
-      <div class="container">
-        <h2>Luca Padovan</h2>
-        <p class="title">Dev</p>
-        <p>Studente presso Università degli Studi di Ferrara</p>
-        <p>luca.padovan@student.unife.it</p>a
-        <p><button class="button">Contatta</button></p>
-      </div>
+  <br />
+  <div class="row" style="width: 1000px; margin: 0 auto;">
+    <div class="col-md-6" style="text-align: center;">
+      <img src="/assets/team/Andrei_Jizdan.jpg" style="border-radius: 50%;" width="250px" class="mx-auto d-block" height="250px" />
+      <h3><span class="badge badge-primary">Andrei Jizdan</span></h3>
+    </div>
+    <div class="col-md-6" style="text-align: center;">
+      <img src="/assets/team/Anonymous.jpg" style="border-radius: 50%;" width="250px" class="mx-auto d-block" height="250px" />
+      <h3><span class="badge badge-primary">Angelo Viro</span></h3>     
     </div>
   </div>
-  <div class="column">
-    <div class="card">
-      <img src="/assets/team/Daniele_Lovato.jpg" alt="John" style="width:100%">
-      <div class="container">
-        <h2>Daniele Lovato</h2>
-        <p class="title">Dev</p>
-        <p>Studente presso Università degli Studi di Ferrara</p>
-        <p>daniele01.lovato@student.unife.it</p>
-        <p><button class="button">Contatta</button></p>
-      </div>
+  <br />
+  <br />
+  <div class="row">
+    <div class="col-md-12" style="text-align: center;">
+      <a class="btn btn-success" href="/" role="button">Ritorna al login</a>
     </div>
   </div>
 </div>
-<div class="row">
-  <div class="column">
-    <div class="card">
-      <img src="/assets/team/Andrei_Jizdan.jpg" alt="Jane" style="width:100%">
-      <div class="container">
-        <h2>Andrei Jizdan</h2>
-        <p class="title">Dev</p>
-        <p>Studente presso Università degli Studi di Ferrara</p>
-        <p>andrei.jizdan@student.unife.it</p>
-        <p><button class="button">Contatta</button></p>
-      </div>
-    </div>
-  </div>
 
-  <div class="column">
-    <div class="card">
-      <img src="/assets/team/Anonymous.jpg" alt="Mike" style="width:100%">
-      <div class="container">
-        <h2>Angelo Viro</h2>
-        <p class="title">Dev</p>
-        <p>Studente presso Università degli Studi di Ferrara</p>
-        <p>angelo.viro@student.unife.it</p>
-        <p><button class="button">Contatta</button></p>
-      </div>
-    </div>
-  </div>
-</div>
+<style type="text/css">
+  .container{
+    position: fixed;
+    width: 100%;
+    top: 90px;
+    left: 0px;
+    right: 0px;
+    bottom: 0px;
+    background: linear-gradient(#FFFFFF, #C5C5C5);
+    overflow: auto;
+  }
+  img {
+    border:1px solid #021a40;
+  }
+</style>
+</body>
 </body>
 </html>
